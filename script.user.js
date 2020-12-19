545703df48140d55f7c20ab6d9a7a64705d931df
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-12-19 10:46:15 -0800

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Bypass Quora Login
+// @namespace    https://github.com/Ray-Adams
+// @version      1.0.0
+// @description  Bypasses the Quora log-in overlay.
+// @author       Ray Adams
+// @match        *://quora.com/*
+// @match        *://www.quora.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(() => {
+
+    'use strict';
+
+    let styles = `
+        .signup_wall_prevent_scroll {
+            overflow:auto!important
+        }
+
+        div#root {
+            filter: none !important;
+        }
+
+        div._DialogSignupForm.BaseSignupForm.vertical_alignment_wrapper {
+            display: none !important;
+        }
+    `, styleSheet = document.createElement("style");
+
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+
+})()
